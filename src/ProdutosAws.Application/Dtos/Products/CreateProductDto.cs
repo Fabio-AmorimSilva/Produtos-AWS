689aae60af02a6201c7f5177d5f6f071cdb6b10c
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2026-05-24 16:41:04 -0300

    refactor: Updating set to init;

--- src/ProdutosAws.Application/Dtos/Products/CreateProductDto.cs
+++ src/ProdutosAws.Application/Dtos/Products/CreateProductDto.cs
@@ -2,6 +2,6 @@
 
 public record CreateProductDto
 {
-    public string Name { get; set; } = string.Empty;
-    public ProductCategory ProductCategory { get; set; }
+    public string Name { get; init; } = string.Empty;
+    public ProductCategory ProductCategory { get; init; }
 }
