9ce46c6e51734444b88fe1e8da1372b5fd272125
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2026-05-24 16:24:33 -0300

    chore: Adding more namespaces

--- src/ProdutosAws.Web/GlobalUsings.cs
+++ src/ProdutosAws.Web/GlobalUsings.cs
@@ -1,5 +1,7 @@
 global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.EntityFrameworkCore;
 global using ProdutosAws.Application;
 global using ProdutosAws.Application.Dtos.Products;
 global using ProdutosAws.Application.Services.Products;
 global using ProdutosAws.Domain.Enums;
+global using ProdutosAws.Infrastructure;
